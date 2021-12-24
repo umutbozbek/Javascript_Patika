dd4827595a3e7ef8a31c97843b556bd6221f8b9a
umutbozbek <umutbozbek58@gmail.com>
2021-12-24 16:52:20 +0300

    Dom

---
+++ DOM_içerisinden_Etiket_ve_ID_ile_Öğe_Seçimi.js
@@ -0,0 +1,14 @@
+
+//let title=document.getElementsByTagName('h2');
+
+let title=document.getElementById('title');
+title.innerHTML='Değişen bilgi'
+console.log(title.innerHTML);
+
+
+let link=document.querySelector("ul#list>li>a");
+link.innerHTML='lik bilgisi değiştirildi';
+console.log(link);
+
+link.style.color='red';
+link.classList.add('btn');
