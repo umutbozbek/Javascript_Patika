373166636638fa2a014e3878f1a4aa0e5b915aa6
umutbozbek <umutbozbek58@gmail.com>
2022-03-31 13:47:59 +0300

    javascript_patika

---
+++ Form_ve_Summit_Yöntemi/form_submit.js
@@ -0,0 +1,13 @@
+
+//form submit
+
+
+let formDom=document.querySelector("#userForm")
+
+formDom.addEventListener("submit",formSubmit);
+
+function formSubmit(event) {
+    event.preventDefault() //default işlemi engelledik.
+    console.log("islem gerçekleşti")
+}
+
